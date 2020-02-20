28ab035901bbb2251b23796918ec134c7cb59bd2
Aluno <Aluno@172.16.90.137>
2020-02-20 10:54:52 -0300

    Aula 20/02

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,31 +1,45 @@
 package controller;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
 @RequestScoped
 public class LoginController {
 
-	private String login = "Maria";
+	private String login;
 	private String senha;
-	
-	public void entrar() {
-		System.out.println(login);
-		System.out.println(senha);
+
+	public String entrar() {
+		if (login.equals("fred") && senha.equals("123")) {
+			return "helloworld.xhtml?faces-redirect=true";
+		}
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login e/ou Senha inválidos.", null));
+		return null;
 	}
-	
+
+	public void limpar() {
+		login = "";
+		senha = "";
+	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
+
 }
